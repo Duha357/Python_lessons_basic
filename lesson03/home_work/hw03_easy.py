3e5f09bf61da3ec791dae25ebcc49c76532c0353
Duha357 <duha-2013@mail.ru>
2018-07-09 21:40:51 +0500

    ДЗ 3 сделано

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,12 +5,33 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
-
-
+    if ndigits <= 0: #Проверяем на правильность ввода округления
+        print("Количество знаков после запятой не должно быть меньше или ровняться нулю!")
+    number = str(number)
+    a = number.split(".") #Делим число на цифры до и после точки
+    b = a[1]
+    c = a[0]
+    if len(b) <= ndigits: #Проверяем, не меньше ли и не равно ли число количеству знаков после запятой
+        b = int(b)
+        return "{}{}{}".format(a[0], ".", b)
+    else:
+        while len(str(b)) == ndigits + 1: #Доводим кол-во знаков до нужного
+            b = int(b) // 10
+        if int(b) % 10 <= 5: #Проверка на округление до 5-ти
+            b = int(b) // 100
+            if len(str(b)) == ndigits + 1: #Проверяем увеличение целой части числа
+                c = int(c) + 1
+                b = 0
+        else: #Проверка на округление от 5-ти
+            b = int(b) // 100 + 1
+            if len(str(b)) == ndigits + 1:
+                c = int(c) + 1
+                b = 0
+        return "{}{}{}".format(c, ".", b)
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
+
 
 
 # Задание-2:
@@ -20,8 +41,13 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
-
+    if ticket_number > 6 and ticket_number < 6: #Проверка на соответствие это легко убрать в коментарии, чтоб не print'ить
+        print("Номер не является шестизначным!")
+    ticket_number = list(map(int, list(str(ticket_number)))
+    if sum(ticket_number[:3]) == sum(ticket_number[3:]): #Проверка суммы правой и левой половины
+        return True
+    else:
+        return False
 
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
