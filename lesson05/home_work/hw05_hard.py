8218700333fd63ae7bff0e76edd17f79f5829b3e
Duha357 <duha-2013@mail.ru>
2018-07-17 17:04:15 +0500

    ДЗ 5 сделано!

--- lesson05/home_work/hw05_hard.py
+++ lesson05/home_work/hw05_hard.py
@@ -13,3 +13,85 @@
 # Исходной директорией считать ту, в которой был запущен скрипт.
 
 # P.S. По возможности, сделайте кросс-платформенную реализацию.
+
+import sys
+import os
+print("sys.argv = ", sys.argv)
+
+def print_help():
+    print("help - получение справки")
+    print("mkdir <dir_name> - создание директории")
+    print("ping - тестовый ключ")
+    print("cp <file_name> - создать копию файла")
+    print("rm <file_name> - удалить файл ")
+    print("cd <full_path> - изменить директорию")
+    print("ls - отобразить полный путь текущей директории")
+def make_dir():
+    if not dir_name:
+        print("Необходимо указать имя файла вторым параметром")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.mkdir(dir_path)
+        print("директория {} создана".format(dir_name))
+    except FileExistsError:
+        print("директория {} уже создана".format(dir_name))
+def ping():
+    print("pong")
+def create_copy():
+    if not file_name:
+        print("Вы забыли имя указать")
+            return
+    a = os.path.join(os.path.dirname(os.path.realpath(__file__)),file_name)
+    try:
+        b = a + ".bak"  # Переименновываем будущую копию
+        shutil.copy(a, b)
+        print(a, "/n", b)  # Выводим пути к файлам исходника и копии
+    except FileExistsError:
+        print("Неуверенность, похоже это не файл")
+
+def delete_file():
+    if not file_name:
+        print("Вы забыли имя указать")
+        return
+
+def move_to_dir():
+    a = input("Введите имя папки, в которую хотите переместиться: ")
+    if not a:
+        print("Вы не ввели имя папки!")
+        return
+    b = os.path.join(os.getcwd(), "{}".format(a))
+    try:
+        os.chdir(b) #Это не работает
+        os.system("cd " + a) #И это не работает, просьба подсказать почему, пытался задать чтоб пользователь вводил
+        # путь до папки, но chdir всё равно не переключает путь директории в командной строке
+        print("Мы перемещены в папку {}!".format(a))
+    except FileNotFoundError:
+        print("Папки нет по этому пути!")
+def we_here():
+    print(os.getcwd())
+
+do = {
+    "help": print_help,
+    "mkdir": make_dir,
+    "ping": ping,
+    "cp": create_copy,
+    "rm": delete_file,
+    "cd": move_to_dir,
+    "ls": we_here
+    }
+try:
+    dir_name = sys.argv[2]
+except IndexError:
+    dir_name = None
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        print("Такой функции не реализовано!")
+        print("Введите help, для справки")
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,76 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import sys
+import os
+import hw05_easy
+
+print("Мы тут:", sys.argv) #Путь к текущей директории.
+
+def help():
+    print("1. Перейти в папку")
+    print("2. Просмотреть содержимое текущей папки")
+    print("3. Удалить папку")
+    print("4. Создать папку")
+
+def move_to_dir():
+    a = input("Введите имя папки, в которую хотите переместиться: ")
+    if not a:
+        print("Вы не ввели имя папки!")
+        return
+    b = os.path.join(os.getcwd(), "{}".format(a))
+    try:
+        os.chdir(b) #Это не работает
+        os.system("cd " + a) #И это не работает, просьба подсказать почему, пытался задать чтоб пользователь вводил
+        # путь до папки, но chdir всё равно не переключает путь директории в командной строке
+        print("Мы перемещены в папку {}!".format(a))
+    except FileNotFoundError:
+        print("Папки нет по этому пути!")
+
+def delete_dir():
+    a = input("Введите имя папки, которую хотите удалить: ")
+    if not a:
+        print("Вы не ввели имя папки!")
+        return
+    b = os.path.join(os.getcwd(), "{}".format(a))
+    try:
+        os.rmdir(b)
+        print("Папка {} успешно удалена!".format(a))
+    except FileExistsError:
+        print("Папки {} нет в этой папке!".format(a))
+
+def create_dir():
+    a = input("Введите имя папки, которую хотите создать: ")
+    if not a:
+        print("Вы не ввели имя папки!")
+        return
+    b = os.path.join(os.getcwd(), "{}".format(a))
+    try:
+        os.mkdir(b)
+        print("Папка {} успешно создана!".format(a))
+    except FileExistsError:
+        print("Папка {} уже существует!".format(a))
+
+do = {
+    "help": help,
+    "1": move_to_dir,
+    "2": hw05_easy.demonstrate, #Как мне объяснил наставник, это единственный скрипт, который можно импортировать.
+    "3": delete_dir,
+    "4": create_dir
+}
+try:
+    dir_name = sys.argv[2]
+except IndexError:
+    dir_name = None
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        print("Такой функции не реализовано!")
+        print("Введите help, для справки")
