90cbc68fddaa3bb229ff0081492086773e2bd369
Duha357 <duha-2013@mail.ru>
2018-07-20 17:40:23 +0500

    ДЗ 6 сделано!

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,78 @@
+import math
+
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 
+class triangle():
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+        self.AB = round(math.sqrt(int(math.fabs(((x2 - x1) ** 2) + ((y2 - y1) ** 2)))), 2)
+        self.BC = round(math.sqrt(int(math.fabs(((x3 - x2) ** 2) + ((y3 - y2) ** 2)))), 2)
+        self.CA = round(math.sqrt(int(math.fabs(((x1 - x3) ** 2) + ((y1 - y3) ** 2)))), 2)
+
+    def perimetr(self):
+        self.perimetr = (self.AB + self.BC + self.CA)
+        return self.perimetr
+
+    def vysota(self):
+        self.x4 = (x1 - x3) / 2  #Находим координаты середины отрезка CA
+        self.y4 = (x1 - y3) / 2
+        self.h = round(math.sqrt(int(math.fabs(((x4 - x2) ** 2) + ((y4 - y2) ** 2)))), 2) #Находим высоту треугольника
+        return self.h
+
+    def ploshad(self):
+        self.polovina_osnovania = self.CA / 2 #Находим длинну половины основания
+        self.ploshad = self.polovina_osnovania * self.h
+        return self.ploshad
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+class trapeze():
+    def __init__(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+        self.x4 = x4
+        self.y4 = y4
+
+    def storoni(self):
+        self.AB = round(math.sqrt(int(math.fabs(((x2 - x1) ** 2) + ((y2 - y1) ** 2)))), 2)
+        self.BC = round(math.sqrt(int(math.fabs(((x3 - x2) ** 2) + ((y3 - y2) ** 2)))), 2)
+        self.CD = round(math.sqrt(int(math.fabs(((x4 - x3) ** 2) + ((y4 - y3) ** 2)))), 2)
+        self.DA = round(math.sqrt(int(math.fabs(((x1 - x4) ** 2) + ((y1 - y4) ** 2)))), 2)
+        return self.AB, self.BC, self.CD, self.DA
+
+    def proverka(self):
+        if self.BC == self.DA and self.AB != self.CD or self.BC != self.DA and self.AB == self.CD:
+            print("Трапеция равнобедренная!")
+        else:
+            print("Трапеция не равнобедренная!")
+
+    def perimetr(self):
+        self.perimetr = (self.AB + self.BC + self.CD + self.DA)
+        return self.perimetr
+
+    def ploshad(self):
+        if self.BC == self.DA and self.AB != self.CD:
+            self.katet = int(math.fabs(self.AB - self.CD)) #Делаем катет бокового треугольника трапеции
+            self.h = round(math.sqrt(int((self.BC ** 2) - (self.katet ** 2))), 2) #Находим высоту трапеции по Пифагору
+            self.polusumma_vershini_i_osnovania = int(self.AB + self.CD) / 2  # Находим полусумму вершины и основания
+            self.ploshad = self.polusumma_vershini_i_osnovania * self.h
+            return self.ploshad
+        elif self.BC != self.DA and self.AB == self.CD:
+            self.katet = int(math.fabs(self.BC - self.DA))  # Делаем катет бокового треугольника трапеции
+            self.h = round(math.sqrt(int((self.AB ** 2) - (self.katet ** 2))), 2)  # Находим высоту трапеции по Пифагору
+            self.polusumma_vershini_i_osnovania = int(self.BC + self.DA) / 2  # Находим полусумму вершины и основания
+            self.ploshad = self.polusumma_vershini_i_osnovania * self.h
+            return self.ploshad
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -15,3 +15,81 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+class School:
+    def __init__(self, class_room):
+        self.class_room = {
+            "class_num": int(class_room.split()[0]),
+            "class_letter": class_room.split()[1]
+        }
+
+    def get_name(self):
+        return str(self.class_room["class_num"]) + " " + self.class_room["class_letter"]
+
+class Person:
+    def __init__(self, name, surname, second_name):
+        self.name = name
+        self.surname = surname
+        self.second_name = second_name
+
+    def get_full_name(self):
+        return self.surname + " " + self.name + " " + self.second_name
+
+
+class Student(Person):
+    def __init__(self, name, surname, second_name, class_room, father, mother):
+        Person.__init__(self, name, surname, second_name)
+        self.class_room = class_room
+        self.father = father
+        self.mother = mother
+
+    def get_class_room(self):
+        return self.class_room
+
+    def get_parents(self):
+        return self.father.get_full_name(), self.mother.get_full_name()
+
+
+class Teacher(Person):
+    def __init__(self, name, surname, second_name, classes, subject):
+        Person.__init__(self, name, surname, second_name)
+        self.classes = classes
+        self.subject = subject
+
+    def get_subject(self):
+        return self.subject
+
+    def get_classes(self):
+        return self.classes
+
+
+class_rooms = ["1 А", "2 Б", "3 В"]
+father = [Person("Иван", "Сидоров", "Игоревич"),
+            Person("Игорь", "Иванов", "Александрович"),
+            Person("Николай", "Петров", "Александрович")]
+mother = [Person("Татьяна", "Сидорова", "Максимовна"),
+            Person("Ирина", "Иванова", "Александровна"),
+            Person("Светлана", "Петрова", "Николаевна")]
+students = [Student("Александр", "Иванов", "Игоревич", class_rooms[0], father[1], mother[1]),
+            Student("Петр", "Сидоров", 'Иванович', class_rooms[1], father[0], mother[0]),
+            Student("Иван", "Петров", 'Николаевич', class_rooms[1], father[2], mother[2]),
+            Student("Алексей", "Иванов", "Игоревич", class_rooms[2], father[1], mother[1]),
+            Student("Евгений", "Сидоров", 'Иванович', class_rooms[2], father[0], mother[0]),
+            Student("Андрей", "Петров", 'Николаевич', class_rooms[2], father[2], mother[2])]
+teachers = [Teacher("Иван", "Сидоров", "Игоревич", [class_rooms[0], class_rooms[1], class_rooms[2]], 'Математика'),
+            Teacher("Игорь", "Иванов", "Александрович", [class_rooms[1], class_rooms[2]], 'История'),
+            Teacher("Николай", "Петров", "Александрович", class_rooms[2], 'Английский')]
+
+#for num, School in enumerate(class_rooms, start=1): #Не хватило времени
+#    print("Все классы: {}) {}".format(num, School.get_name()))
+
+for num, Student in enumerate(students, start=1):
+    print("Все ученики в классах: {}) {} {}".format(num, Student.get_class_room(), Student.get_full_name()))
+
+#На реализацию списка всх предметов ученика не хватило времени
+
+for num, Parents in enumerate(students, start=1):
+    print("Родители всех учеников: {}) Ученик: {} Родители: {}".format(num, Student.get_full_name(), Parents.get_parents()))
+
+for num, Teacher in enumerate(teachers, start=1):
+    print("Список учителей: {}) Учитель: {} Препадаёт в классе: {}".format(num, Teacher.get_full_name(), Teacher.get_classes()))
