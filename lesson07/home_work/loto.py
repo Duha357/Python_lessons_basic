e22d83d383b967a49ce7e35b92feb23952f1a42d
Duha357 <duha-2013@mail.ru>
2018-07-23 22:04:14 +0500

    ДЗ 7 сделано!

--- lesson07/home_work/loto.py
+++ lesson07/home_work/loto.py
@@ -57,3 +57,101 @@
 модуль random: http://docs.python.org/3/library/random.html
 
 """
+
+import random
+import sys
+import time
+
+class Barrel:
+    def __init__(self, remainder):
+        self.remainder = remainder
+        self.gen = self.generator_of_barrel()
+
+    def generator_of_barrel(self):
+        a = [x for x in range(1, self.remainder + 1)]
+        random.shuffle(a)
+        for i, y in enumerate(a):
+            print("Новый бочонок: {} (осталось {})".format(y, self.remainder - (i + 1)))
+            yield y
+
+
+class Ticket:
+    def __set_card(self):
+        num = set()
+        while len(num) < self.all_row * 5:
+            num.add(random.randint(1, 91))
+        cards = list(num)
+        random.shuffle(cards)
+
+        while len(cards) % self.all_row != 0:
+            cards.append("None")
+        self.all_row = int(len(cards) / self.all_row)
+        cards = [cards[i: i + self.all_row] for i in range(0, len(cards), self.all_row)]
+
+        for i in range(len(cards)):
+            cards[i].sort()
+        self.card_user = cards[:3]
+        self.card_comp = cards[3:]
+
+    def __init__(self, amount_card):
+        row = 3
+        self.all_row = row * amount_card
+        self.__set_card()
+
+    def get_card(self, card_player):
+        print("{:-^25}".format(self.name))
+        print("{0[0]:>2} {0[1]:<10} {0[2]:<5} {0[3]} {0[4]} ".format(card_player[0]))
+        print("{0[0]:>4} {0[1]:<6} {0[2]:<4} {0[3]:<4} {0[4]} ".format(card_player[1]))
+        print("{0[0]} {0[1]:<5} {0[2]:<5} {0[3]:<5} {0[4]} ".format(card_player[2]))
+        print("{:-^25}".format("-"))
+
+    # Поиск номера на карточке и определение победителя
+    def search(self, card_player, num_barrel):
+        for i, n in enumerate(card_player):
+            if num_barrel in n:
+                card_player[i][n.index(num_barrel)] = "-"
+                self.score += 1
+                if self.score == 15:
+                    print("{} победил!".format(self.name))
+                    sys.exit(1)
+                return True
+        return False
+
+
+class Player(Ticket):
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+
+def main(): #Основной интерфейс
+    ticket = Ticket(2)
+    barrel = Barrel(90)
+    player1 = Player(" Ваша карточка ")
+    player2 = Player(" Карточка компьютера ")
+
+    while True:
+        num_barrel = next(barrel.gen)
+        player1.get_card(ticket.card_user)
+        player2.get_card(ticket.card_comp)
+
+        inp_user = input("Зачеркнуть цифру? (y/n)")
+        if inp_user == "y":
+            if player1.search(ticket.card_user, num_barrel):
+                continue
+            else:
+                print("Игра окончена!")
+                sys.exit(1)
+        if inp_user == "n":
+            if player1.search(ticket.card_user, num_barrel):
+                print('Игра окончена!')
+                sys.exit(1)
+            elif player2.search(ticket.card_comp, num_barrel):
+                continue
+        if inp_user != "n" and inp_user != "y":
+            print("Введите y or n")
+            time.sleep(1)
+            continue
+
+if __name__ == "__main__":
+    main()
